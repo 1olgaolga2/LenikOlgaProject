7eb9626c275ff0b17d89709071376ff0a1aa5a4d
1olgaolga2 <99136275+1olgaolga2@users.noreply.github.com>
2025-12-17 13:10:27 +0100

    add beforeEach, afterEach

--- tests/fixtures.ts
+++ tests/fixtures.ts
@@ -31,4 +31,13 @@ export const test = base.extend<TestFixtures>({
   },
 });
 
+test.beforeEach(async ({ loginPage }) => {
+  await loginPage.goto();
+  // additionally here should be implemented steps like user registration via API, login via cookies etc. Now it's inside my tests. 
+});
+
+test.afterEach(async () => {
+  // here should be implemented cleanup steps - e.g. deleting users which has been created during tests
+});
+
 export { expect } from '@playwright/test';
